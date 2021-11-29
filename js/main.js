f5b0e55340c8a04e3c8207a8ac36f71e741efcca
Georgy Bashirov <goshan397@gmail.com>
2021-11-29 21:51:12 +0300

    new index predicted.html

--- js/main.js
+++ js/main.js
@@ -10,36 +10,5 @@ $(function () {
         }, "slow");
     });
 
-    $('.header__form-btn').on('click', function () {
-        $('.modal').css('display', 'block');
-    });
-
-    $('.close__modal').on('click', function () {
-        $('.modal').css('display', 'none');
-    });
-
 });
 
-const yesBtn = document.querySelector('.modal-question__yes');
-const noBtn = document.querySelector('.modal-question__no');
-const modal = document.querySelector('.modal');
-const modalQuestion = document.querySelector('.modal-question');
-const modalContent = document.querySelector('.modal-content');
-
-modal.onclick = function (e) {
-    if (e.target === modal) {
-        modalQuestion.style.display = 'block';
-        modalContent.style.opacity = '0.1';
-    }
-}
-
-yesBtn.onclick = function () {
-    modalQuestion.style.display = 'none';
-    modal.style.display = 'none';
-    window.location.reload();
-}
-
-noBtn.onclick = function () {
-    modalQuestion.style.display = 'none';
-    modalContent.style.opacity = '1';
-}
